3e568c8a0ef0790f992147844add179856b43de3
Tadas <tadas.auciunas@gmail.com>
2015-04-28 15:26:56 +0300

    new / popular books controllers added

--- src/Nfq/LibraryBundle/Controller/HomepageController.php
+++ src/Nfq/LibraryBundle/Controller/HomepageController.php
@@ -14,31 +14,47 @@ class HomepageController extends Controller
 {
     public function indexAction()
     {
-       // $newBooks = $this->getNewestBooks();
-        $popularBooks = $this->getPopularBooks();
+        return $this->render('base.html.twig');
+    }
+
+    public function newBooksAction()
+    {
+        $booksRepo = $this->getDoctrine()->getRepository('NfqLibraryBundle:Books');
+
+        $query = $booksRepo->createQueryBuilder('b')
+            ->select('b, MIN(b.addedAt) as addedAt')
+            ->orderBy('addedAt', 'DESC')
+            ->setMaxResults(6)
+            ->groupBy('b.description')
+            ->getQuery();
+
+        $bookArray = array();
+        foreach ($query->getResult() as $books) {
+            array_push($bookArray, $books[0]);
+        }
+
         return $this->render('default/index.html.twig', array(
-            "books" => $popularBooks));
+            "books" => $bookArray));
     }
 
-    private function getNewestBooks()
+    public function popularBooksAction()
     {
-        $descriptionsRepo = $this->getDoctrine()->getRepository('NfqLibraryBundle:Descriptions');
-        $query = $descriptionsRepo->createQueryBuilder('d')
-            ->orderBy('d.addedAt', 'DESC')
+        $booksRepo = $this->getDoctrine()->getRepository('NfqLibraryBundle:Orders');
+
+        $query = $booksRepo->createQueryBuilder('o')
+            ->select('o, COUNT(o.book) as bookCount')
+            ->orderBy('bookCount', 'DESC')
             ->setMaxResults(6)
+            ->groupBy('o.book')
             ->getQuery();
 
-        return $query->getResult();
-    }
+        $bookArray = array();
+        foreach ($query->getResult() as $books) {
+            array_push($bookArray, $books[0]->getBook());
+        }
 
-    private function getPopularBooks()
-    {
-        $descriptionsRepo = $this->getDoctrine()->getRepository('NfqLibraryBundle:Descriptions');
-        $query = $descriptionsRepo->createQueryBuilder('d')
-            ->orderBy('d.takenCount', 'DESC')
-            ->setMaxResults(6)
-            ->getQuery();
+        return $this->render('default/index.html.twig', array(
+            "books" => $bookArray));
 
-        return $query->getResult();
     }
 }
