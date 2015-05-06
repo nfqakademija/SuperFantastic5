fef74ddd1993f90512e85627ea28c2db467fc29c
Ignas Jatulis <ijatulis@gmail.com>
2015-05-07 00:10:30 +0300

    Base template edit

--- src/Nfq/LibraryBundle/Resources/public/js/javascript.js
+++ src/Nfq/LibraryBundle/Resources/public/js/javascript.js
@@ -4,3 +4,12 @@
 function goBack() {
     window.history.back();
 }
+
+$(function()
+{
+    $("a#toggle").click(function()
+    {
+        $("#orders").slideToggle();
+        return false;
+    });
+});
